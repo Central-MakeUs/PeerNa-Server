6d720b80ac7e95cd5bdb83f95b9daac4af0ba346
HyoBN <qls0786@naver.com>
2024-01-05 16:48:46 +0900

    :tada: Project Setting

---
+++ src/main/java/cmc/peerna/PeernaApplication.java
@@ -0,0 +1,13 @@
+package cmc.peerna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeernaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeernaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cmc/peerna/PeernaApplicationTests.java
@@ -0,0 +1,13 @@
+package cmc.peerna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeernaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
