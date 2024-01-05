13dd80599b23b166d12bd84d2e625186621e790b
HyoBN <qls0786@naver.com>
2024-01-05 18:47:54 +0900

    healthCheck 추가

---
+++ src/main/java/cmc/peerna/web/controller/RootController.java
@@ -0,0 +1,13 @@
+package cmc.peerna.web.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RootController {
+
+    @GetMapping("/health")
+    public String healthCheck() {
+        return "I'm health!";
+    }
+}
