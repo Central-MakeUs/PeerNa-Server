fd8c04d4ff6ee03a66544891026e076e448bbe2d
HyoBN <qls0786@naver.com>
2024-01-24 21:51:19 +0900

    :hammer: Fix: 마이페이지-종합평가 응답 수정

--- src/main/java/cmc/peerna/service/RootService.java
+++ src/main/java/cmc/peerna/service/RootService.java
@@ -8,7 +8,6 @@ import java.util.List;
 
 public interface RootService {
     List<Long> getcolorAnswerIdList(Member member, List<Long> selfTestAnswerIdList);
-    List<TestResponseDto.totalEvaluation> getTop3TotalEvaluation(Member member);
     RootResponseDto.MypageDto getMyPageDto(Member member);
 
     List<TestResponseDto.totalEvaluation> getTotalEvaluationList(Member member);
